fd8077eb62aa73e169e7b610dfd0b0157f20fd9e
UPHSD Molino CCS OJT <uphsdmolinoccs.ojt@gmail.com>
2026-02-11 14:58:52 +0800

    Delete dist folder

--- dist/middleware/auth.d.ts
+++
@@ -1,5 +0,0 @@
-import { Response, NextFunction } from 'express';
-import { IRequest } from '../types';
-export declare const isAuthenticated: (req: IRequest, res: Response, next: NextFunction) => void | Response;
-export declare const isGuest: (req: IRequest, res: Response, next: NextFunction) => void | Response;
-//# sourceMappingURL=auth.d.ts.map
