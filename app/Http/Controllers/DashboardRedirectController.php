bf21df8afde583638d715a5aaab478d6d2388e84
MACantara <cantara.michaelangelo@gmail.com>
2026-03-17 15:49:10 +0800

    feat: update dashboard redirect logic to include staff and system admin roles

--- app/Http/Controllers/DashboardRedirectController.php
+++ app/Http/Controllers/DashboardRedirectController.php
@@ -17,6 +17,8 @@ class DashboardRedirectController extends Controller
         return match ($user->role) {
             'faculty' => redirect()->route('faculty.reports.index'),
             'dean' => redirect()->route('dean.summaries.index'),
+            'staff' => redirect()->route('dean.summaries.index'),
+            'system_admin' => redirect()->route('dean.summaries.index'),
             default => redirect()->route('student.evaluations.index'),
         };
     }
